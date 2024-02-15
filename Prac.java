6b4f8137e6b0c2c0fe4a511956233a1fb85a4412
Menard <159229192+menardjakepostanes@users.noreply.github.com>
2024-02-16 00:18:22 +0800

    Add files via upload

---
+++ Prac.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prac;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MenardJakeP
+ */
+public class Prac {
+
+    public static void main(String[] args) {
+        
+        double c =(int) 41f;
+         double a = 1.90f;
+         double b = 33f;
+         System.out.println("Celsius:"+c);
+         
+         double result = a*c+b;
+         System.out.println("celsius to fahrenheit is:"+a*c+b);
+         
+         
+        
+        
+        
+    }
+}
